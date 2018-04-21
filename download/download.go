fd0aa31a4ff33c4233c8fa2cf4778aea566fb7b0
Yonathan Koren <yonkornilov@live.com>
2018-04-21 16:37:32 -0400

    download package initial commit

---
+++ download/download.go
@@ -0,0 +1,28 @@
+package download
+
+import (
+	"http"
+	"io"
+	"os"
+)
+
+func Download(path string, url string) error {
+	out, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
