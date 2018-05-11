2348bf2f31f5fb7b413d4f68bc863d356dc3e7a8
Yonathan Koren <yonkornilov@live.com>
2018-05-11 08:50:27 -0400

    delegate struct funcs for Runner extract and download

--- runner/runner.go
+++ runner/runner.go
@@ -14,23 +14,39 @@ type Runner struct {
 func (r *Runner) Run() error {
 	c := r.Config
 	for _, p := range c.Packages {
-		downloadPath := p.GetDownloadPath()
-		filePath := p.GetBinaryPath()
-		err := download.Run(p, downloadPath)
+		downloadPath, err := r.Download(p)
 		if err != nil {
 			return err
 		}
-		err = extract.Run(p, downloadPath, filePath)
+		binaryPath, err := r.Extract(p, downloadPath)
 		if err != nil {
 			return err
 		}
-		err = os.Chmod(filePath, 0700)
+		err = os.Chmod(binaryPath, 0700)
 		if err != nil {
 			return err
 		}
 	}
 
 	return nil
+}
+
+func (r *Runner) Download(p config.Package) (downloadPath string, err error) {
+	downloadPath = p.GetDownloadPath()
+	err = download.Run(p, downloadPath)
+	if err != nil {
+		return "", err
+	}
+	return downloadPath, nil
+}
+
+func (r *Runner) Extract(p config.Package, downloadPath string) (binaryPath string, err error) {
+	binaryPath = p.GetBinaryPath()
+	err = extract.Run(p, downloadPath, binaryPath)
+	if err != nil {
+		return "", err
+	}
+	return binaryPath, nil
 }
 
 func Make(config config.Config) Runner {
