3928ff7e4dfe3eef970d98d25d83df132acc840d
Yonathan Koren <yonkornilov@live.com>
2018-05-13 22:45:37 -0400

    fix extract message

--- extract/extract.go
+++ extract/extract.go
@@ -13,7 +13,7 @@ import (
 func Run(b config.Binary, downloadPath string) (binaryPath string, err error) {
 	binaryPath = b.GetBinaryPath()
 
-	fmt.Printf("Extracting %s from %s\n", b.SrcType, b.Src)
+	fmt.Printf("Extracting %s from %s\n", b.SrcType, downloadPath)
 	err, extractedPath := extract(b.SrcType, downloadPath)
 	if err != nil {
 		return "", err
