44d2506041ae39131de53f9517014d20c3397ede
Yonathan Koren <yonkornilov@live.com>
2018-05-14 22:04:14 -0400

    fix config test and add binary tests

--- config/binary_test.go
+++ config/binary_test.go
@@ -5,64 +5,12 @@ import (
 	"testing"
 )
 
-func TestConfigGood(t *testing.T) {
+func TestConfigBinaryMissingName(t *testing.T) {
 	config := `
 packages:
   - name: vault
     binaries:
-      - name: vault
-        mode: 0755
-        src: https://releases.hashicorp.com/vault/0.10.0/vault_0.10.0_linux_amd64.zip
-        format: zip
-    files:
-      - path: /etc/init.d/vault
-        mode: 0700
-        content: |
-          #!/sbin/openrc-run
-
-          NAME=vault
-          DAEMON=/usr/bin/$NAME
-
-          depend() {
-                  need net
-                  after firewall
-          }
-
-          start() {
-                  ebegin "Starting ${NAME}"
-                          start-stop-daemon --start \
-                                  --background \
-                                  --make-pidfile --pidfile /var/run/$NAME.pid \
-                                  --stderr "/var/log/$NAME.log" \
-                                  --stdout "/var/log/$NAME.log" \
-                                  --user $USER \
-                                  --exec $DAEMON \
-                                  -- \
-                                  -config /etc/vault/config.hcl
-                  eend $?
-          }
-
-          stop () {
-                  ebegin "Stopping ${NAME}"
-                          start-stop-daemon --stop \
-                                  --pidfile /var/run/$NAME.pid \
-                                  --user $USER \
-                                  --exec $DAEMON
-                  eend $?
-          }
-`
-	_, err := New([]byte(config))
-	if err != nil {
-		t.Fatal("Cannot validate config: " + err.Error())
-	}
-}
-
-func TestConfigMissingName(t *testing.T) {
-	config := `
-packages:
-  - binaries:
-      - name: vault
-        mode: 0755
+      - mode: 0755
         src: https://releases.hashicorp.com/vault/0.10.0/vault_0.10.0_linux_amd64.zip
         format: zip
     files:
@@ -109,10 +57,14 @@ packages:
 	t.Log(fmt.Sprintf("Got expected error: \n%s", err.Error()))
 }
 
-func TestConfigMissingBinaries(t *testing.T) {
+func TestConfigBinaryMissingMode(t *testing.T) {
 	config := `
 packages:
   - name: vault
+    binaries:
+      - name: vault
+        src: https://releases.hashicorp.com/vault/0.10.0/vault_0.10.0_linux_amd64.zip
+        format: zip
     files:
       - path: /etc/init.d/vault
         mode: 0700
@@ -157,7 +109,59 @@ packages:
 	t.Log(fmt.Sprintf("Got expected error: \n%s", err.Error()))
 }
 
-func TestConfigMissingFiles(t *testing.T) {
+func TestConfigMissingSrc(t *testing.T) {
+	config := `
+packages:
+  - name: vault
+    binaries:
+      - name: vault
+        mode: 0755
+        format: zip
+    files:
+      - path: /etc/init.d/vault
+        mode: 0700
+        content: |
+          #!/sbin/openrc-run
+
+          NAME=vault
+          DAEMON=/usr/bin/$NAME
+
+          depend() {
+                  need net
+                  after firewall
+          }
+
+          start() {
+                  ebegin "Starting ${NAME}"
+                          start-stop-daemon --start \
+                                  --background \
+                                  --make-pidfile --pidfile /var/run/$NAME.pid \
+                                  --stderr "/var/log/$NAME.log" \
+                                  --stdout "/var/log/$NAME.log" \
+                                  --user $USER \
+                                  --exec $DAEMON \
+                                  -- \
+                                  -config /etc/vault/config.hcl
+                  eend $?
+          }
+
+          stop () {
+                  ebegin "Stopping ${NAME}"
+                          start-stop-daemon --stop \
+                                  --pidfile /var/run/$NAME.pid \
+                                  --user $USER \
+                                  --exec $DAEMON
+                  eend $?
+          }
+`
+	_, err := New([]byte(config))
+	if err == nil {
+		t.Fatal("Expected validation error, got nothing.")
+	}
+	t.Log(fmt.Sprintf("Got expected error: \n%s", err.Error()))
+}
+
+func TestConfigMissingFormat(t *testing.T) {
 	config := `
 packages:
   - name: vault
@@ -165,7 +169,42 @@ packages:
       - name: vault
         mode: 0755
         src: https://releases.hashicorp.com/vault/0.10.0/vault_0.10.0_linux_amd64.zip
-        format: zip
+    files:
+      - path: /etc/init.d/vault
+        mode: 0700
+        content: |
+          #!/sbin/openrc-run
+
+          NAME=vault
+          DAEMON=/usr/bin/$NAME
+
+          depend() {
+                  need net
+                  after firewall
+          }
+
+          start() {
+                  ebegin "Starting ${NAME}"
+                          start-stop-daemon --start \
+                                  --background \
+                                  --make-pidfile --pidfile /var/run/$NAME.pid \
+                                  --stderr "/var/log/$NAME.log" \
+                                  --stdout "/var/log/$NAME.log" \
+                                  --user $USER \
+                                  --exec $DAEMON \
+                                  -- \
+                                  -config /etc/vault/config.hcl
+                  eend $?
+          }
+
+          stop () {
+                  ebegin "Stopping ${NAME}"
+                          start-stop-daemon --stop \
+                                  --pidfile /var/run/$NAME.pid \
+                                  --user $USER \
+                                  --exec $DAEMON
+                  eend $?
+          }
 `
 	_, err := New([]byte(config))
 	if err == nil {
