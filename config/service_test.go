41c9269d2d1d4f6701c265733decde2e00ba76ba
Yonathan Koren <yonkornilov@live.com>
2018-05-26 15:12:53 -0400

    add missing binary test

--- config/service_test.go
+++ config/service_test.go
@@ -25,3 +25,21 @@ packages:
 	}
 	t.Log(fmt.Sprintf("Got expected error: \n%s", err.Error()))
 }
+
+func TestConfigFileNoArgs(t *testing.T) {
+	config := `
+packages:
+  - name: vault
+    binaries:
+      - name: vault
+        mode: 0755
+        src: https://releases.hashicorp.com/vault/0.10.0/vault_0.10.0_linux_amd64.zip
+        format: zip
+    services:
+      - binary: vault
+`
+	_, err := New([]byte(config))
+	if err != nil {
+		t.Fatal("Cannot validate service: " + err.Error())
+	}
+}
