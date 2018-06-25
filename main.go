d64390a481a92b6cbd61b4fd4c5e4b6789b246fd
Yonathan Koren <yonkornilov@live.com>
2018-06-25 18:41:56 -0400

    switch on runner error to check for exec.ExitError; use log.Fatalf instead of panic

--- main.go
+++ main.go
@@ -4,24 +4,36 @@ import (
 	"github.com/yonkornilov/snowcapper/config"
 	"github.com/yonkornilov/snowcapper/context"
 	"github.com/yonkornilov/snowcapper/runner"
+
+	"log"
+	"os/exec"
+)
+
+const (
+	runtimeErrFormat string = "Runtime error: %s\n"
 )
 
 func main() {
 	configYaml, err := Asset("config.yaml")
 	if err != nil {
-		panic(err)
+		log.Fatalf(runtimeErrFormat, err)
 	}
 	conf, err := config.New(configYaml)
 	if err != nil {
-		panic(err)
+		log.Fatalf(runtimeErrFormat, err)
 	}
 	ctx := context.New(false)
 	r, err := runner.New(&ctx, conf)
 	if err != nil {
-		panic(err)
+		log.Fatalf(runtimeErrFormat, err)
 	}
 	err = r.Run()
 	if err != nil {
-		panic(err)
+		switch err.(type) {
+		default:
+			log.Fatalf(runtimeErrFormat, err)
+		case *exec.ExitError:
+			log.Fatalf(runtimeErrFormat, err.(*exec.ExitError).Stderr)
+		}
 	}
 }
