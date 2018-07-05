bda8e8950a65de94eb0042b00b04610967d074fb
Yonathan Koren <yonkornilov@live.com>
2018-07-04 20:21:01 -0400

    add tests for each minimal config containg only a single type of package attribute

--- config/pkg_test.go
+++ config/pkg_test.go
@@ -164,3 +164,72 @@ packages:
 	}
 	t.Log(fmt.Sprintf("Got expected error: \n%s", err.Error()))
 }
+
+func TestConfigOnlyBinaries(t *testing.T) {
+	config := `
+packages:
+  - name: vault
+    binaries:
+      - name: vault
+        mode: 0755
+        src: https://releases.hashicorp.com/vault/0.10.0/vault_0.10.0_linux_amd64.zip
+        format: zip
+`
+	_, err := New([]byte(config))
+	if err != nil {
+		t.Fatal("Cannot validate config: " + err.Error())
+	}
+}
+
+func TestConfigOnlyFiles(t *testing.T) {
+	config := `
+packages:
+  - name: vault
+    files:
+      - path: /etc/vault/config.hcl
+        mode: 0700
+        content: |
+          storage "file" {
+            path    = "/mnt/vault/data"
+          }
+
+          listener "tcp" {
+            address     = "0.0.0.0:8200"
+            tls_disable = 1
+          }
+`
+	_, err := New([]byte(config))
+	if err != nil {
+		t.Fatal("Cannot validate config: " + err.Error())
+	}
+}
+
+func TestConfigOnlyServices(t *testing.T) {
+	config := `
+packages:
+  - name: vault
+    services:
+      - binary: vault
+        args:
+          - "server"
+          - "-config /etc/vault/config.hcl"
+`
+	_, err := New([]byte(config))
+	if err != nil {
+		t.Fatal("Cannot validate config: " + err.Error())
+	}
+}
+
+func TestConfigOnlyInits(t *testing.T) {
+	config := `
+packages:
+  - name: vault
+    inits:
+      - type: command
+        content: echo test
+`
+	_, err := New([]byte(config))
+	if err != nil {
+		t.Fatal("Cannot validate config: " + err.Error())
+	}
+}
