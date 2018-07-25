d44203468282d391c4c969c59ffe9ea591451a5c
Yonathan Koren <yonkornilov@live.com>
2018-07-24 21:56:44 -0400

    test empty hash

--- config/binary_test.go
+++ config/binary_test.go
@@ -417,6 +417,40 @@ packages:
 	}
 }
 
+func TestConfigEmptyHash(t *testing.T) {
+	config := `
+packages:
+  - name: vault
+    binaries:
+      - name: vault
+        mode: 0755
+        src: https://releases.hashicorp.com/vault/0.10.0/vault_0.10.0_linux_amd64.zip
+        src_hash: "" 
+        format: zip
+    files:
+      - path: /etc/vault/config.hcl
+        mode: 0700
+        content: |
+          storage "file" {
+            path    = "/mnt/vault/data"
+          }
+
+          listener "tcp" {
+            address     = "0.0.0.0:8200"
+            tls_disable = 1
+          }
+    services:
+      - binary: vault
+        args:
+          - "server"
+          - "-config /etc/vault/config.hcl"
+`
+	_, err := New([]byte(config))
+	if err != nil {
+		t.Fatal("Cannot validate config: " + err.Error())
+	}
+}
+
 func TestConfigBadHashLength(t *testing.T) {
 	config := `
 packages:
