895a062f21af85b607e03cd3b054cf4a89bd9156
Yonathan Koren <yonkornilov@live.com>
2018-07-25 00:20:42 -0400

    add tests for checkHashSumIfExists for sha384

--- download/download_test.go
+++ download/download_test.go
@@ -44,3 +44,25 @@ func TestCheckHashIfExistsSha512Bad(t *testing.T) {
 		t.Fatalf("Expected error, got nothing")
 	}
 }
+
+func TestCheckHashIfExistsSha384(t *testing.T) {
+	testBinary, err := testasset.Asset("test.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = checkHashIfExists(testBinary, "109bb6b5b6d5547c1ce03c7a8bd7d8f80c1cb0957f50c4f7fda04692079917e4f9cad52b878f3d8234e1a170b154b72d")
+	if err != nil {
+		t.Fatalf("Expected no error, got %s", err)
+	}
+}
+
+func TestCheckHashIfExistsSha384Bad(t *testing.T) {
+	testBinary, err := testasset.Asset("test.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = checkHashIfExists(testBinary, "f09bb6b5b6d5547c1ce03c7a8bd7d8f80c1cb0957f50c4f7fda04692079917e4f9cad52b878f3d8234e1a170b154b72d")
+	if err == nil {
+		t.Fatalf("Expected error, got nothing")
+	}
+}
