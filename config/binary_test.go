bc5144884b936dc8a3f6d29266bb07609e54eb0c
Yonathan Koren <yonkornilov@live.com>
2018-07-24 21:52:05 -0400

    test sha256 config

--- config/binary_test.go
+++ config/binary_test.go
@@ -281,6 +281,40 @@ packages:
 	}
 }
 
+func TestConfigSha256(t *testing.T) {
+	config := `
+packages:
+  - name: vault
+    binaries:
+      - name: vault
+        mode: 0755
+        src: https://releases.hashicorp.com/vault/0.10.0/vault_0.10.0_linux_amd64.zip
+        src_hash: 1234567890123456789012345678901234567890123456789012345678901234 
+        format: zip
+    files:
+      - path: /etc/vault/config.hcl
+        mode: 0700
+        content: |
+          storage "file" {
+            path    = "/mnt/vault/data"
+          }
+
+          listener "tcp" {
+            address     = "0.0.0.0:8200"
+            tls_disable = 1
+          }
+    services:
+      - binary: vault
+        args:
+          - "server"
+          - "-config /etc/vault/config.hcl"
+`
+	_, err := New([]byte(config))
+	if err != nil {
+		t.Fatal("Cannot validate config: " + err.Error())
+	}
+}
+
 func TestConfigBadHashLength(t *testing.T) {
 	config := `
 packages:
